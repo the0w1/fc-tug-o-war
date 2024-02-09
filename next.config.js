d7a3854b90591c3d9746f286bf6b7687141a509b
the0w1 <the0w1@proton.me>
2024-02-09 14:58:28 -0800

    Adding next config with path configuration for fixing canvas dependency requirements with nextjs https://github.com/Automattic/node-canvas/issues/1779\#issuecomment-895885846

---
+++ next.config.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+}
+ 
+module.exports = nextConfig
+
