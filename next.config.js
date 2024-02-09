7dd7f38889a1be146a411274efa5f4782e96b224
the0w1 <the0w1@proton.me>
2024-02-09 15:08:48 -0800

    Adding env variables to cron handler

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 
+/** @type {import('next').NextConfig} */
 if (
   process.env.LD_LIBRARY_PATH == null ||
   !process.env.LD_LIBRARY_PATH.includes(
@@ -10,11 +11,8 @@ if (
     process.env.PWD
   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
 }
- 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
 }
  
 module.exports = nextConfig
-
